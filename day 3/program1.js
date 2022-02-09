3d85854b4a13fc02f390a7017139471c0a0d3b52
ykaransingha <99267351+ykaransingha@users.noreply.github.com>
2022-02-09 21:43:54 +0530

    Add files via upload

---
+++ day 3/program1.js
@@ -0,0 +1,7 @@
+    // program to print the table of 5
+    var num;
+    for(let i=1; i<=10; i++)
+    {
+        num=5*i;
+        console.log(num);
+    }
---
+++ day 3/program2.js
@@ -0,0 +1,6 @@
+// 5 variables holding marks of 5 subjects
+// calculate the percentage
+var sub1 = 60, sub2 = 80, sub3 = 70, sub4 = 90, sub5 = 60, per;
+per = (sub1 + sub2 + sub3 + sub4 + sub5)*(100/500);
+console.log("The percentage is printed below:-"); 
+console.log(per);
